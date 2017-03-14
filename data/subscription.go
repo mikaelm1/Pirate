108879282d94689ad88b330d05d3ac3658b4010d
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2017-03-13 17:06:52 -0700

    Deleted github related files

--- data/subscription.go
+++
@@ -1,8 +0,0 @@
-package data
-
-// Subscription model
-type Subscription struct {
-	Subscribed bool   `json:"subscribed"`
-	CreatedAt  string `json:"created_at"`
-	RepoURL    string `json:"repository_url"`
-}
