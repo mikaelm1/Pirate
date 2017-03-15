154e7f62996d496cfa338e4a24c04aaafddca416
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2017-03-15 13:33:08 -0700

    Added command and api call to get all images

--- service/user.go
+++ service/user.go
@@ -2,28 +2,29 @@ package service
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 
 	"github.com/mikaelm1/pirate/data"
 )
 
 // GetUserInfo gets info about the user
-func (c *DOService) GetUserInfo() error {
+func (c *DOService) GetUserInfo(u *data.Account) error {
 	res, err := c.MakeGETRequest("https://api.digitalocean.com/v2/account")
 	if err != nil {
 		return err
 	}
-	var account data.Account
+	// var account data.Account
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		panic(err.Error())
 	}
 	res.Body.Read(body)
-	err = json.Unmarshal(body, &account)
+	fmt.Println(string(body))
+	err = json.Unmarshal(body, &u)
 	if err != nil {
 		// fmt.Println("Error: ", err)
 		return err
 	}
-	account.PrintInfo()
 	return nil
 }
