2cbb3ba25bbe9304fa03e70c38fa158665cb0824
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2017-03-14 19:14:22 -0700

    Added api call to create load balancer

--- service/balancers.go
+++ service/balancers.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"net/http"
 
 	"encoding/json"
@@ -25,5 +26,36 @@ func (c *DOService) FetchAllLoadBalancers(balancers *data.LoadBalancers) (*http.
 		return res, err
 	}
 	// fmt.Println(string(body))
+	return res, nil
+}
+
+// CreateLoadBalancer sends post request to create a new load balancer
+func (c *DOService) CreateLoadBalancer(balancer *data.LoadBalancer) (*http.Response, error) {
+	body, err := json.Marshal(balancer)
+	if err != nil {
+		return nil, err
+	}
+	res, err := c.MakePostRequest("https://api.digitalocean.com/v2/load_balancers", body)
+	if err != nil {
+		return res, err
+	}
+	body, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	res.Body.Read(body)
+	if res.StatusCode == 422 {
+		return res, fmt.Errorf("Status Code 422: Make sure all protocols are on the same network layer")
+	}
+	if res.StatusCode >= 300 {
+		return res, fmt.Errorf("Status Code %d: There was an error creating the load balancer", res.StatusCode)
+	}
+	balancers := data.LoadBalancers{
+		Balancers: []data.LoadBalancer{*balancer},
+	}
+	err = json.Unmarshal(body, &balancers)
+	if err != nil {
+		return res, err
+	}
 	return res, nil
 }
